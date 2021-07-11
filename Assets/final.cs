f86525c2e9394703adb56a5286fe3c3e7922a35f
SirineMaghraoui <maghraoui.sirine@gmail.com>
2021-07-11 23:58:17 +0100

    First commit

---
+++ Assets/final.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class final : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return)){
+            SceneManager.LoadScene("scene1");
+        }
+   }
+
+    // Update is called once per frame
+  
+}
